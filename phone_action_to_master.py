d889e874172e9ab745023d70e9bee27f69565b35
Paht J <paht.juangphanich@gmail.com>
2023-06-27 20:44:14 -0400

    latest code
    need to make a branch for cleaning up Just had to change column names from one sheet to the next.

--- phone_action_to_master.py
+++ phone_action_to_master.py
@@ -28,15 +28,15 @@ def validNumber(phone_number):
     return True
 
 if __name__=="__main__":
-    master_workbook='PB_CLE_CRM.xlsx'
+    master_workbook='master_clean.xlsx'
     master_worksheet='PB CLE Master List'
     
-    workbooks_to_combine = ['PB_CLE_CRM.xlsx']
-    worksheets_to_combine = ['Jan 9th RSVP']
+    workbooks_to_combine = ['Phone2ActionExport_Advocate_2023-05-24-11-51.xlsx']
+    worksheets_to_combine = ['Phone2ActionExport_Advocate_202']
     
     master = pd.ExcelFile(master_workbook)
     df_master = master.parse(master_worksheet)
-
+    
     for i in range(len(workbooks_to_combine)):
         book = pd.ExcelFile(workbooks_to_combine[i])
         df_sheet = book.parse(worksheets_to_combine[i])
@@ -66,12 +66,12 @@ if __name__=="__main__":
         # mEmails2 = list(map(str.strip, list(df_master_clean['Email 2'])))
         # mPhones = list(map(str.strip, list(df_master_clean['Phone'])))
 
-        df_sheet_clean = df_sheet.dropna(subset=["First name","Last name"]) # Drop Nan for lastname and firstname
-        df_sheet_clean["Phone"].replace(np.nan, '', regex=True,inplace=True)
-        df_sheet_clean["Email"].replace(np.nan, '', regex=True,inplace=True)
-        df_sheet_clean["Phone"] = df_sheet_clean["Phone"].astype("string")
-        df_sheet_clean["Email"] = df_sheet_clean["Email"].astype("string")
-        df_sheet_clean["I live in Cleveland Ward # "].replace(np.nan, -1, regex=True,inplace=True)
+        df_sheet_clean = df_sheet.dropna(subset=["First Name","Last Name"]) # Drop Nan for lastname and firstname
+        df_sheet_clean["Phone 1"].replace(np.nan, '', regex=True,inplace=True)
+        df_sheet_clean["Email 1"].replace(np.nan, '', regex=True,inplace=True)
+        df_sheet_clean["Phone 1"] = df_sheet_clean["Phone 1"].astype("string")
+        df_sheet_clean["Email 1"] = df_sheet_clean["Email 1"].astype("string")
+        df_sheet_clean["City District"].replace(np.nan, -1, regex=True,inplace=True)
         # sFirstNames = list(map(str.strip, list(df_sheet_clean[column_map['First Name']])))
         # sLastNames = list(map(str.strip, list(df_sheet_clean[column_map['Last Name']])))
         # sWards = list(map(str.strip, list(df_sheet_clean['I live in Cleveland Ward #'])))
@@ -89,9 +89,9 @@ if __name__=="__main__":
         data_to_add = list()                # Add missing index
         changes = list()
         for i in range(len(df_sheet_clean)):
-            FirstName = df_sheet_clean.iloc[i]["First name"].strip().lower()
-            LastName = df_sheet_clean.iloc[i]["Last name"].strip().lower()
-            Ward = df_sheet_clean.iloc[i]["Last name"].strip().lower()
+            FirstName = df_sheet_clean.iloc[i]["First Name"].strip().lower()
+            LastName = df_sheet_clean.iloc[i]["Last Name"].strip().lower()
+            Ward = df_sheet_clean.iloc[i]["Last Name"].strip().lower()
             # Searchs for missing data based on first name lastname
             temp = df_master_clean.index[
                                 (df_master_clean['First Name'].str.strip().str.lower() == FirstName) & 
@@ -107,49 +107,49 @@ if __name__=="__main__":
             if len(temp)>0: # Data exists
                 change = {"First Name":FirstName, "Last Name":LastName}
                 j = temp[0]
-                if df_sheet_clean.iloc[i]["Phone"].strip() != df_master_clean.iloc[j]["Phone"].strip():
+                if df_sheet_clean.iloc[i]["Phone 1"].strip() != df_master_clean.iloc[j]["Phone"].strip():
                     change["Phone Past Value"] = df_master_clean.iloc[j]["Phone"]
                     try:
-                        phone = phonenumbers.format_number(phonenumbers.parse(df_sheet_clean.iloc[i]["Phone"], 'US'),'US')
+                        phone = phonenumbers.format_number(phonenumbers.parse(df_sheet_clean.iloc[i]["Phone 1"], 'US'),'US')
                     except:
-                        phone = df_sheet_clean.iloc[i]["Phone"]
+                        phone = df_sheet_clean.iloc[i]["Phone 1"]
                     change["Phone New Value"] = phone
-                    df_master_clean.iloc[j]["Phone"] = phone
+                    df_master_clean.iloc[j]["Phone "] = phone
                     
-                if df_sheet_clean.iloc[i]["Email"].strip() != df_master_clean.iloc[j]["Email"]:
+                if df_sheet_clean.iloc[i]["Email 1"].strip() != df_master_clean.iloc[j]["Email"]:
                     change["Email Past Value"] = df_master_clean.iloc[j]["Email"]
-                    change["Email New Value"] = (parseaddr(df_sheet_clean.iloc[i]["Email"])[1] if parseaddr(df_sheet_clean.iloc[i]["Email"]) else "")
-                    df_master_clean.iloc[j]["Email"] = (parseaddr(df_sheet_clean.iloc[i]["Email"])[1] if parseaddr(df_sheet_clean.iloc[i]["Email"]) else "")
+                    change["Email New Value"] = (parseaddr(df_sheet_clean.iloc[i]["Email 1"])[1] if parseaddr(df_sheet_clean.iloc[i]["Email 1"]) else "")
+                    df_master_clean.iloc[j]["Email"] = (parseaddr(df_sheet_clean.iloc[i]["Email 1"])[1] if parseaddr(df_sheet_clean.iloc[i]["Email 1"]) else "")
                 
                 try:
-                    newWard = int(df_sheet_clean.iloc[i]["I live in Cleveland Ward # "])
+                    newWard = int(df_sheet_clean.iloc[i]["City District"])
                 except:
                     newWard = -1
-                if df_master_clean.iloc[j]["Ward"] != df_sheet_clean.iloc[i]["I live in Cleveland Ward # "] and newWard>0:
+                if df_master_clean.iloc[j]["Ward"] != df_sheet_clean.iloc[i]["City District"] and newWard>0:
                     change["Ward Past Value"] = df_master_clean.iloc[j]["Ward"]
-                    change["Ward New Value"] = df_sheet_clean.iloc[i]["I live in Cleveland Ward # "]
-                    df_master_clean.iloc[j]["Ward"] = df_sheet_clean.iloc[i]["I live in Cleveland Ward # "]
+                    change["Ward New Value"] = df_sheet_clean.iloc[i]["City District"]
+                    df_master_clean.iloc[j]["Ward"] = df_sheet_clean.iloc[i]["City District"]
                 
                 if "Address" in df_sheet_clean:
-                    if df_master_clean.iloc[j]["Address"] != df_sheet_clean.iloc[i]["Address"].strip():
-                        df_master_clean.iloc[j]["Address"] = df_sheet_clean.iloc[i]["Address"]
+                    if df_master_clean.iloc[j]["Address"] != df_sheet_clean.iloc[i]["Address 1"].strip():
+                        df_master_clean.iloc[j]["Address"] = df_sheet_clean.iloc[i]["Address 1"]
                 changes.append(change)
             else:
                 data_to_add.append({
                         'First Name':df_sheet_clean.iloc[i][column_map['First Name']],
                         'Last Name':df_sheet_clean.iloc[i][column_map['Last Name']],
-                        "Phone":df_sheet_clean.iloc[i]['Phone'],
-                        "Email":df_sheet_clean.iloc[i]['Email'],
-                        "Ward":df_sheet_clean.iloc[i]["I live in Cleveland Ward # "],
-                        "Source":"Jan 9th"
+                        "Phone":df_sheet_clean.iloc[i]['Phone 1'],
+                        "Email":df_sheet_clean.iloc[i]['Email 1'],
+                        "Ward":df_sheet_clean.iloc[i]["City District"],
+                        "Source":df_sheet_clean.iloc[i]["Source Campaign Id"]
                     })
             
         
-        pd.concat([df_master_clean,pd.DataFrame(data_to_add)],ignore_index=False).to_excel('master_clean.xlsx','PB CLE Master List')
+        pd.concat([df_master_clean,pd.DataFrame(data_to_add)],ignore_index=False).to_excel('master_clean_w_phone.xlsx','PB CLE Master List')
         
 
-        pd.DataFrame(changes).to_excel("master_clean_changes.xlsx")
-        pd.DataFrame(data_to_add).to_excel("master_clean_data_added.xlsx")
+        pd.DataFrame(changes).to_excel("master_clean_changes_w_phone.xlsx")
+        pd.DataFrame(data_to_add).to_excel("master_clean_data_added_w_phone.xlsx")
         print('done')
          
         
